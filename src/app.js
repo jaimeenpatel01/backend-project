d0164d99b782bddbab34b78c7df79f380c1888ca
git <jaimeenpatel2017@gmail.com>
2024-08-28 12:07:40 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,5 +12,11 @@ app.use(cors({
 app.use(express.json({limit:"19kb"}))
 app.use(express.urlencoded({limit:"19kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+
+
+app.use("/api/v1/users",userRouter)
 
 export { app }
